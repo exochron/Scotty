ea0b0549960024b34b8290a8355cd84882973f77
exochron <exochron@freenet.de>
2025-12-06 16:19:48 +0100

    chore: updated screenshots

--- DevZone/MoveScreenshots.sh
+++ DevZone/MoveScreenshots.sh
@@ -5,5 +5,5 @@ cd Images || return
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 02_options.jpg
 mv "$(ls -dtr1 ../../../../Screenshots/* | tail -1)" 01_menu.jpg
 
-magick 01_menu.jpg -crop 600x433+2125+29 01_menu.jpg
+magick 01_menu.jpg -crop 600x567+2090+29 01_menu.jpg
 magick 02_options.jpg -crop 799x450+1770+130 02_options.jpg
