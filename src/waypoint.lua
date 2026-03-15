c7981406588d647dfd5c1e451cdc599cbe597e05
exochron <exochron@freenet.de>
2026-03-15 10:29:30 +0100

    fix: cleanup TomTom waypoints after 2 zone changes

--- src/waypoint.lua
+++ src/waypoint.lua
@@ -7,11 +7,22 @@ ADDON.Events:RegisterCallback("TeleportInitialized", function(_, _, _, dbRow)
             local opts = {
                 title = ADDON:GetName(dbRow),
                 from = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Title"),
+                crazy = true,
             }
             local ttUId = TomTom:AddWaypoint(dbRow.waypoint[1],dbRow.waypoint[2],dbRow.waypoint[3], opts)
-            -- todo: clear waypoint on reached mapid
+            -- clear waypoint after reaching 2 zones
+            local handle
+            local triggeredZoneChange = 0
+            handle = ADDON.Events:RegisterFrameEventAndCallbackWithHandle("ZONE_CHANGED_NEW_AREA", function()
+                triggeredZoneChange = triggeredZoneChange + 1
+                if triggeredZoneChange >= 2 then
+                    TomTom:RemoveWaypoint(ttUId)
+                    handle:Unregister()
+                end
+            end, ttUId)
         else
-            -- try adding normal waypoint
+            -- todo: add normal waypoints
         end
+        --todo test with waypoint UI
     end
 end, "waypoint")
