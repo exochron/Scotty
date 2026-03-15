d2d26f96b7e6ab5f149fc93a66f743254fa92324
exochron <exochron@freenet.de>
2026-03-15 11:19:53 +0100

    feat: added support for ingame waypoints where possible

--- src/waypoint.lua
+++ src/waypoint.lua
@@ -1,15 +1,18 @@
 local ADDON_NAME, ADDON = ...
 
--- waypoint support for TomTom
+-- waypoint support
 ADDON.Events:RegisterCallback("TeleportInitialized", function(_, _, _, dbRow)
     if dbRow and dbRow.waypoint then
+        local mapID = dbRow.waypoint[1]
+        local posX = dbRow.waypoint[2]
+        local posY = dbRow.waypoint[3]
+
         if TomTom then
             local opts = {
                 title = ADDON:GetName(dbRow),
                 from = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Title"),
-                crazy = true,
             }
-            local ttUId = TomTom:AddWaypoint(dbRow.waypoint[1],dbRow.waypoint[2],dbRow.waypoint[3], opts)
+            local ttUId = TomTom:AddWaypoint(mapID, posX, posY, opts)
             -- clear waypoint after reaching 2 zones
             local handle
             local triggeredZoneChange = 0
@@ -20,9 +23,12 @@ ADDON.Events:RegisterCallback("TeleportInitialized", function(_, _, _, dbRow)
                     handle:Unregister()
                 end
             end, ttUId)
-        else
-            -- todo: add normal waypoints
+        elseif C_Map.CanSetUserWaypointOnMap and C_Map.CanSetUserWaypointOnMap(mapID) then
+            local uiMapPoint = UiMapPoint.CreateFromCoordinates(mapID, posX, posY)
+            if uiMapPoint then
+                C_Map.SetUserWaypoint(uiMapPoint)
+                C_SuperTrack.SetSuperTrackedUserWaypoint(true)
+            end
         end
-        --todo test with waypoint UI
     end
 end, "waypoint")
